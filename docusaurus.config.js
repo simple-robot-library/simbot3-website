11eb9bc44d0c66288e88209cecd0518434c11770
ForteScarlet <ForteScarlet@163.com>
2022-04-24 09:31:48 +0800

    公告提示不可关闭

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -88,7 +88,7 @@ const config = {
       // 公告
       announcementBar: {
         id: 'still_under_construction',
-        content: `🚧 <b>尚在施工中, 内容仅供参考</b>`,
+        content: `🚧 <b>尚在施工中, 内容仅供参考</b> 🚧`,
         backgroundColor: '#FFB906',
         textColor: '#142F48',
         isCloseable: false
