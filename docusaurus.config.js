35cfb02803b2f7f1c584ec0769c6346a64576da6
ForteScarlet <ForteScarlet@163.com>
2023-02-13 17:51:32 +0800

    build: 移除 pwa 支持

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,69 +53,69 @@ async function createConfig() {
             // https://github.com/flexanalytics/plugin-image-zoom
             'plugin-image-zoom',
             // https://docusaurus.io/zh-CN/docs/api/plugins/@docusaurus/plugin-pwa
-            [
-                '@docusaurus/plugin-pwa',
-                {
-                    // debug: true,
-                    offlineModeActivationStrategies: [
-                        'appInstalled',
-                        // 'standalone',
-                        'queryString',
-                    ],
-                    pwaHead: [
-                        {
-                            tagName: 'link',
-                            rel: 'icon',
-                            href: '/img/logo.png',
-                        },
-                        {
-                            tagName: 'link',
-                            rel: 'manifest',
-                            href: '/manifest.json',
-                        },
-                        // {
-                        //   tagName: 'meta',
-                        //   name: 'theme-color',
-                        //   content: 'rgb(37, 194, 160)',
-                        // },
-                        {
-                            tagName: 'meta',
-                            name: 'apple-mobile-web-app-capable',
-                            content: 'yes',
-                        },
-                        {
-                            tagName: 'meta',
-                            name: 'apple-mobile-web-app-status-bar-style',
-                            content: '#000',
-                        },
-                        {
-                            tagName: 'link',
-                            rel: 'apple-touch-icon',
-                            href: '/img/logo.png',
-                        },
-                        // {
-                        //   tagName: 'link',
-                        //   rel: 'mask-icon',
-                        //   href: '/img/logo.png',
-                        //   color: 'rgb(37, 194, 160)',
-                        // },
-                        {
-                            tagName: 'meta',
-                            name: 'msapplication-TileImage',
-                            content: '/img/logo.png',
-                        },
-                        {
-                            tagName: 'meta',
-                            name: 'msapplication-TileColor',
-                            content: '#000',
-                        },
-                        // {
-                        //   tagName: 'meta',
-                        //   name: 'theme-color',
-                        //   content: 'rgb(37, 194, 160)',
-                        // },
-                    ],
-                }],
+            // [
+            //     '@docusaurus/plugin-pwa',
+            //     {
+            //         // debug: true,
+            //         offlineModeActivationStrategies: [
+            //             'appInstalled',
+            //             // 'standalone',
+            //             'queryString',
+            //         ],
+            //         pwaHead: [
+            //             {
+            //                 tagName: 'link',
+            //                 rel: 'icon',
+            //                 href: '/img/logo.png',
+            //             },
+            //             {
+            //                 tagName: 'link',
+            //                 rel: 'manifest',
+            //                 href: '/manifest.json',
+            //             },
+            //             // {
+            //             //   tagName: 'meta',
+            //             //   name: 'theme-color',
+            //             //   content: 'rgb(37, 194, 160)',
+            //             // },
+            //             {
+            //                 tagName: 'meta',
+            //                 name: 'apple-mobile-web-app-capable',
+            //                 content: 'yes',
+            //             },
+            //             {
+            //                 tagName: 'meta',
+            //                 name: 'apple-mobile-web-app-status-bar-style',
+            //                 content: '#000',
+            //             },
+            //             {
+            //                 tagName: 'link',
+            //                 rel: 'apple-touch-icon',
+            //                 href: '/img/logo.png',
+            //             },
+            //             // {
+            //             //   tagName: 'link',
+            //             //   rel: 'mask-icon',
+            //             //   href: '/img/logo.png',
+            //             //   color: 'rgb(37, 194, 160)',
+            //             // },
+            //             {
+            //                 tagName: 'meta',
+            //                 name: 'msapplication-TileImage',
+            //                 content: '/img/logo.png',
+            //             },
+            //             {
+            //                 tagName: 'meta',
+            //                 name: 'msapplication-TileColor',
+            //                 content: '#000',
+            //             },
+            //             // {
+            //             //   tagName: 'meta',
+            //             //   name: 'theme-color',
+            //             //   content: 'rgb(37, 194, 160)',
+            //             // },
+            //         ],
+            //     }],
             // '常见问题' 文档
             [
                 'content-docs',
